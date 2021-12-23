138edb002ae5488bd3e2af006ad44a9f856c91c0
tygerxqt <tygerxqt@gmail.com>
2021-12-23 02:52:11 +0000

    add ban command

--- src/Commands/Moderation/Ban.ts
+++ src/Commands/Moderation/Ban.ts
@@ -1,4 +1,6 @@
 import { Command } from "../../Interfaces/Command";
+import { Permissions } from "discord.js";
+import { MessageEmbed } from "discord.js";
 
 export const command: Command = {
     name: 'ban',
@@ -7,6 +9,22 @@ export const command: Command = {
     aliases: [],
     visable: true,
     run: async (client, message, args) => {
-        message.react('⚠️');
+        if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS || Permissions.FLAGS.ADMINISTRATOR)) return message.channel.send('Error! You don\'t have the correct permissions to use this command.');
+
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0].toString());
+        console.log(member);
+        let reason = args.slice(1).join(' ') || 'No reason provided';
+        if (!member) return message.channel.send('Error! You need to specify a user to ban.');
+        if (!member.bannable) return message.channel.send('Error! I don\'t have the correct permissions to ban this user.');
+
+        try {
+            await member.ban({ reason: reason });
+            const res = new MessageEmbed()
+                .setDescription(`Successfully banned \`${member.user.tag}\``);
+            message.channel.send({ embeds: [res] });
+        } catch (err) {
+            console.error(err);
+            message.channel.send('Error! An error occured while trying to kick this user.');
+        }
     }
 }

8cd10b4acdbadf1f1799444716eada5af24ca95f
tygerxqt <tygerxqt@gmail.com>
2021-12-31 15:25:11 +0000

    Update Ban.ts

--- src/Commands/Moderation/Ban.ts
+++ src/Commands/Moderation/Ban.ts
@@ -1,6 +1,7 @@
 import { Command } from "../../Interfaces/Command";
 import { Permissions } from "discord.js";
 import { MessageEmbed } from "discord.js";
+import ErrorEmbed from "../../Embeds/ErrorEmbed";
 
 export const command: Command = {
     name: 'ban',
@@ -9,22 +10,59 @@ export const command: Command = {
     aliases: [],
     visable: true,
     run: async (client, message, args) => {
-        if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS || Permissions.FLAGS.ADMINISTRATOR)) return message.channel.send('Error! You don\'t have the correct permissions to use this command.');
+        if (!message.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS || Permissions.FLAGS.ADMINISTRATOR)) return message.channel.send('You don\'t have the correct permissions to use this command.');
 
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0].toString());
-        console.log(member);
-        let reason = args.slice(1).join(' ') || 'No reason provided';
-        if (!member) return message.channel.send('Error! You need to specify a user to ban.');
-        if (!member.bannable) return message.channel.send('Error! I don\'t have the correct permissions to ban this user.');
+        // mentions
+        if (args[0].includes('<@')) {
+            const member = message.mentions.members.first();
+            const reason = args.slice(1).join(' ') || 'No reason provided';
 
-        try {
-            await member.ban({ reason: reason });
-            const res = new MessageEmbed()
-                .setDescription(`Successfully banned \`${member.user.tag}\``);
-            message.channel.send({ embeds: [res] });
-        } catch (err) {
-            console.error(err);
-            message.channel.send('Error! An error occured while trying to kick this user.');
+            if (!member) return message.channel.send('This member isn\'t in the server. Please try banning them with their user ID instead.');
+            if (!member.bannable) return message.channel.send('I am not able to ban users that have higher privileges than me.');
+            try {
+                await member.ban({ reason: reason });
+                let res = `Successfully banned \`${member.user.tag}\``;
+                if (reason != 'No reason provided') {
+                    res += ` with reason: \`${reason}\``;
+                }
+
+                const embed = new MessageEmbed()
+                    .setDescription(res)
+                message.channel.send({ embeds: [embed] });
+            } catch (err) {
+                console.log(err);
+                ErrorEmbed(message, 'Failed to ban that member. The error has been logged. Please try again later.');
+            }
+        // ID
+        } else {
+            const reg = new RegExp('^[0-9]*$');
+            if (reg.test(args[0]) === false) return message.channel.send('Please provide a valid ID.');
+            const reason = args.slice(1).join(' ') || 'No reason provided';
+
+            await message.guild.members.fetch(args[0]).then(async (member) => {
+                if (!member.bannable) return message.channel.send('I am unable to ban a member with higher privileges than me.');
+
+                try {
+                    member.ban({ reason: reason });
+                } catch (err) {
+                    console.log(err);
+                    ErrorEmbed(message, 'Failed to ban that member. The error has been logged. Please try again later.');
+                }
+
+                let res = `Successfully banned \`${member.user.tag}\``;
+                if (reason != 'No reason provided') {
+                    res += ` with reason: \`${reason}\``;
+                }
+                
+                const embed = new MessageEmbed()
+                    .setDescription(res)
+                    .setColor('#f3aca0');
+                message.channel.send({ embeds: [embed] });
+
+            }).catch(err => {
+                console.log(err);
+                return message.channel.send('I could\'t find that member. Please check if the ID your provided is correct.');
+            });
         }
     }
 }

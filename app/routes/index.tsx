11972994ae042340e5c6ab1671da95c1aa444f42
NyanSpaghetti <47715070+NyanSpaghetti@users.noreply.github.com>
2022-06-23 18:29:09 +0900

    Remix-Express setup

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+        Remix Docs
+      </a>
+    </div>
+  )
+}

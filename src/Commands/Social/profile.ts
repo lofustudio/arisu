99c55007d14c966b5f79333f4f2ab2c4799d4559
Ty Mason <hi@tygr.dev>
2022-07-18 23:48:00 +0100

    Remove console.log

--- src/Commands/Social/profile.ts
+++ src/Commands/Social/profile.ts
@@ -15,7 +15,6 @@ export const command: DiscordCommand = {
         if (!args[0]) {
             const guildData = await client.database.guild.findUnique({ where: { id: message.guild?.id }, include: { economy: true, social: true } });
             const currencyData = guildData?.economy?.currency as unknown as CurrencyJSON;
-            console.log(currencyData);
             const embed = new MessageEmbed()
                 .setAuthor({ iconURL: message.author.displayAvatarURL({ dynamic: true, size: 512 }), name: `${message.author.tag}'s Profile` })
             // Add Pixel data here for tyger's valley https://tygr.dev/discord

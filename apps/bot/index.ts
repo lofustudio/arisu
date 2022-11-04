a3748e24adb67b0986a93c0c347c786c368b1702
Ty Mason <tygerxqt@proton.me>
2022-11-04 23:44:51 +0000

    Move placement of the .env.example
    Co-authored-by: Nyan Spaghetti <NyanSpaghetti@users.noreply.github.com>

--- apps/bot/index.ts
+++ apps/bot/index.ts
@@ -6,7 +6,7 @@ import Client from "./Modules/Client";
 import { Intents } from "discord.js";
 import axios from "axios";
 
-access(".env", constants.R_OK, async (err) => {
+access("./.env", constants.R_OK, async (err) => {
   if (err) {
     interface IAnswer {
       token: string;
@@ -14,92 +14,93 @@ access(".env", constants.R_OK, async (err) => {
     }
 
     try {
-      axios.get("https://raw.githubusercontent.com/lofustudio/arisu/main/.env.example").then((res) => {
-        writeFileSync(".env.example", res.data);
+      access("../../.env.example", constants.W_OK, (err) => {
+        if (err) {
+          axios.get("https://raw.githubusercontent.com/lofustudio/arisu/main/apps/bot/.env.example").then((res) => {
+            writeFileSync("../../.env.example", res.data);
+          });
+        }
       });
-    } catch (e) {
-      console.log(e);
+
+      const questions = new Enquirer<IAnswer>();
+
+      await questions
+        .prompt([
+          {
+            type: "password",
+            name: "token",
+            message: "Please enter your bot token.",
+            required: true,
+            async validate(value: string) {
+              const { Client } = require("discord.js");
+              const client = new Client({ intents: [] });
+              const result: string = client
+                .login(value)
+                .then(() => {
+                  return true;
+                })
+                .catch(() => {
+                  return "Invalid token.";
+                });
+              return result;
+            },
+          },
+          {
+            type: "input",
+            name: "database_url",
+            message: "Please enter your database url (PostgreSQL only).",
+            required: true,
+            initial: "postgresql://",
+            async validate(value) {
+              const { Client } = require("pg");
+              const client = new Client({
+                connectionString: value,
+                connectionTimeoutMillis: 5000,
+              });
+
+              const result: string = client
+                .connect()
+                .then(() => {
+                  client.end();
+                  return true;
+                })
+                .catch((err: Error) => {
+                  return "Database connection failed. Please check your database URL and try again.";
+                });
+
+              return result;
+            },
+          },
+        ])
+        .then((answers: IAnswer) => {
+          writeFileSync(
+            ".env",
+            Buffer.from(
+              `TOKEN=${answers.token}\nDATABASE_URL=${answers.database_url}`
+            )
+          );
+        })
+        .catch((err: Error) => {
+          console.log(err);
+          process.exit(1);
+        });
+
+      exec("prisma generate", (err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+          return process.exit(1);
+        }
+      });
+
+      console.log("Successfully created .env file. Restarting...");
+      const child = exec("node ./index.js");
+    } catch (err) {
+      console.error(err);
+      return process.exit(1);
     }
-
-    const questions = new Enquirer<IAnswer>();
-
-    await questions
-      .prompt([
-        {
-          type: "password",
-          name: "token",
-          message: "Please enter your bot token.",
-          required: true,
-          async validate(value: string) {
-            const { Client } = require("discord.js");
-            const client = new Client({ intents: [] });
-            const result: string = client
-              .login(value)
-              .then(() => {
-                return true;
-              })
-              .catch(() => {
-                return "Invalid token.";
-              });
-            return result;
-          },
-        },
-        {
-          type: "input",
-          name: "database_url",
-          message: "Please enter your database url (PostgreSQL only).",
-          required: true,
-          initial: "postgresql://",
-          async validate(value) {
-            const { Client } = require("pg");
-            const client = new Client({
-              connectionString: value,
-              connectionTimeoutMillis: 5000,
-            });
-
-            const result: string = client
-              .connect()
-              .then(() => {
-                client.end();
-                return true;
-              })
-              .catch((err: Error) => {
-                return "Database connection failed. Please check your database URL and try again.";
-              });
-
-            return result;
-          },
-        },
-      ])
-      .then((answers: IAnswer) => {
-        writeFileSync(
-          ".env",
-          Buffer.from(
-            `TOKEN=${answers.token}\nDATABASE_URL=${answers.database_url}`
-          )
-        );
-      })
-      .catch((err: Error) => {
-        console.log(err);
-        process.exit(1);
-      });
-
-    exec("prisma generate", (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-        return process.exit(1);
-      }
-    });
-
-    console.log("Successfully created .env file. Restarting...");
-    const child = exec("node ./index.js");
-  } catch (err) {
-    console.error(err);
-    return process.exit(1);
+  } else {
+    start();
   }
-} else {
-  start();
-}
 });
 
 dotenv.config();
